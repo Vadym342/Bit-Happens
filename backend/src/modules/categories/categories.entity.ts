2972b22678f383e58b9d17c09521d95a706c093c
Nick <nkur11516@gmail.com>
2025-01-14 13:24:37 +0200

    EP-41: Create table Categories

---
+++ backend/src/modules/categories/categories.entity.ts
@@ -0,0 +1,52 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'categories' })
+export class Category {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'category_id',
+  })
+  id: string;
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: 50,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    name: 'description',
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+  })
+  description: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    nullable: true,
+  })
+  updatedAt: Date | null;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamptz',
+    nullable: true,
+  })
+  deletedAt: Date | null;
+
+  @Column('uuid', {
+    name: 'administrator_id',
+    nullable: true,
+  })
+  administratorId: string;
+}
