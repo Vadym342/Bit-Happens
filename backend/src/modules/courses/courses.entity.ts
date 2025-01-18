7364d984721b794e601582c7144375692800c933
Nick <nkur11516@gmail.com>
2025-01-18 15:56:37 +0200

    EP 37: Create table Courses

---
+++ backend/src/modules/courses/courses.entity.ts
@@ -0,0 +1,83 @@
+import { Category } from '@modules/categories/categories.entity';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'courses' })
+export class Course {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'course_id',
+  })
+  id: string;
+
+  @Column({
+    name: 'title',
+    type: 'varchar',
+    length: 150,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    name: 'description',
+    type: 'text',
+    nullable: false,
+  })
+  description: string;
+
+  @Column('uuid', {
+    name: 'teacher_id',
+  })
+  teacherId: string;
+
+  @Column({
+    name: 'content',
+    type: 'text',
+    nullable: false,
+  })
+  content: string;
+
+  @Column({
+    name: 'rating',
+    type: 'real',
+    nullable: false,
+  })
+  rating: number;
+
+  @Column({
+    name: 'logo_image',
+    type: 'varchar',
+    length: 50,
+    nullable: true,
+  })
+  logoImage: string;
+
+  @Column({
+    name: 'price',
+    type: 'money',
+    nullable: false,
+  })
+  price: number;
+
+  @ManyToOne(() => Category, (category) => category.id)
+  categoryId: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    nullable: true,
+  })
+  updatedAt: Date | null;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamptz',
+    nullable: true,
+  })
+  deletedAt: Date | null;
+}
